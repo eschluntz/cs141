686c4eecbce527ce31b75d8a1878f3e0f5cb05f5
Lucy Cheng <lcheng@college.harvard.edu>
2014-10-31 13:18:29 -0400

    stuff

--- visual2/touchpad_controller.v
+++ visual2/touchpad_controller.v
@@ -49,23 +49,12 @@ always @(posedge cclk) begin
 
 	// Reset module
 	if(~rstb) begin
-		x <= 12'd0;
-		y <= 12'd0;
-		z <= 12'd0;
-		last_data <= 12'd0;
-		sum_data <= 15'd0;
-		
 		touch_clk <= 0;
 		clk_div_counter <= 0;
-		counter_num_requests <= 0;
-		counter_per_request <= 0;
-		counter_type <= 0;
-		touch_csb <= 1;
 	end
 	
 	// Beginning of main code
 	else begin
-		touch_csb <= 0;
 		
 		// incrementing our clock division
 		if(clk_div_counter != (`TOUCH_CLK_DIV_COUNT-1)) begin
@@ -89,55 +78,68 @@ end
 	counter_per_request: our state going through the current request
 */
 always @(negedge touch_clk) begin
-			
-	if (counter_type == 0) begin
-		data_out <= x_request[counter_per_request];
-	end else if (counter_type == 1)  begin
-		data_out <= y_request[counter_per_request];
-	end else if (counter_type == 2) begin
-		data_out <= z_request[counter_per_request];
-	end
-
-	if (counter_per_request == 5'd19) begin // finished a request
+	
+	if(~rstb) begin
+		x <= 12'd0;
+		y <= 12'd0;
+		z <= 12'd0;
+		last_data <= 12'd0;
+		sum_data <= 15'd0;
+		counter_num_requests <= 0;
 		counter_per_request <= 0;
-		
-		// take data and add to average
-		sum_data <= sum_data + last_data;
-		last_data <= 0;
-		
-		if (counter_num_requests == 4'd7) begin // finished all requests in group
-			counter_num_requests <= 0;
-			
-			// take average from all requests and return data
-			if (counter_type == 0) begin //            // x
-				counter_type <= counter_type + 1;
-				x <= sum_data[14:3];
-				sum_data <= 0;
-			end else if (counter_type == 1) begin //   // y
-				counter_type <= counter_type + 1;
-				y <= sum_data[14:3];
-				sum_data <= 0;
-			end else begin //	                         // z  
-				counter_type <= 0;
-				z <= sum_data[14:3];
-				sum_data <= 0;
-			end
-
-		end else begin // next request
-			counter_num_requests <= counter_num_requests + 1;
-		end
-	end else begin // next time step
-		counter_per_request <= counter_per_request + 1;
+		counter_type <= 0;
+		touch_csb <= 1;
+		data_out <= 0;
 	end
-end
+	
+	// Beginning of main code
+	else begin
+		touch_csb <= 0;
+		// we can read on negedge since its still stable
+		// have we reached the data yet?
+		if (counter_per_request > 5'd8) begin
+			last_data[counter_per_request - 5'd9] <= data_in;
+		end
 
-/* pos edge logic
-	responsible for reading in the data sent back by the touchpad
-*/
-always @(posedge touch_clk) begin
-	// have we reached the data yet?
-	if (counter_per_request > 5'd8) begin
-		last_data[counter_per_request - 5'd9] <= data_in;
+		if (counter_type == 0) begin
+			data_out <= x_request[counter_per_request];
+		end else if (counter_type == 1)  begin
+			data_out <= y_request[counter_per_request];
+		end else if (counter_type == 2) begin
+			data_out <= z_request[counter_per_request];
+		end
+
+		if (counter_per_request == 5'd19) begin // finished a request
+			counter_per_request <= 0;
+			
+			// take data and add to average
+			sum_data <= sum_data + last_data;
+			last_data <= 0;
+			
+			if (counter_num_requests == 4'd7) begin // finished all requests in group
+				counter_num_requests <= 0;
+				
+				// take average from all requests and return data
+				if (counter_type == 0) begin //            // x
+					counter_type <= counter_type + 1;
+					x <= sum_data[14:3];
+					sum_data <= 0;
+				end else if (counter_type == 1) begin //   // y
+					counter_type <= counter_type + 1;
+					y <= sum_data[14:3];
+					sum_data <= 0;
+				end else begin //	                         // z  
+					counter_type <= 0;
+					z <= sum_data[14:3];
+					sum_data <= 0;
+				end
+
+			end else begin // next request
+				counter_num_requests <= counter_num_requests + 1;
+			end
+		end else begin // next time step
+			counter_per_request <= counter_per_request + 1;
+		end
 	end
 end
 
