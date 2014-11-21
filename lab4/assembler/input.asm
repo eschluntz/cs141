851e31867df34b2367dd6e8137aca790104da7f6
Lucy Cheng <lcheng@college.harvard.edu>
2014-11-21 00:17:03 -0500

    simple test

--- lab4/assembler/input.asm
+++ lab4/assembler/input.asm
@@ -1,5 +1,6 @@
         addi $t1, $zero, 0
         jal loop
-        sw        $t1,    40($zero)
+        j end
 loop:   addi      $t1,    $t1, 4        # $s0 = $s1 + $s2
         jr $ra
+end:    sw        $t1,    40($zero)
