4f5dd3e3af98a6b8e9ebd3f103a1cb2e615488ec
Erik Schluntz <eschluntz@gmail.com>
2014-11-21 02:20:04 -0500

    more R testing

--- lab4/assembler/math.asm
+++ lab4/assembler/math.asm
@@ -1,13 +1,27 @@
-addi	$t0, $zero, 17
-addi	$t1, $zero, 42
-add	$s0, $t0, $t1
-sub	$s1, $t1, $t0
-or	$s2, $t0, $t1
-and	$s3, $t0, $t1
-xor	$s4, $t0, $t1
+addi	$t0, $zero, 14 # E
+addi	$t1, $zero, 7 # hex answers:
 
-sw	$s0, 60($zero)
-sw	$s1, 64($zero)
-sw	$s2, 68($zero)
-sw	$s3, 72($zero)
-sw	$s4, 76($zero)
+and	$s0, $t0, $t1 # 6
+or	$s1, $t0, $t1 # F
+xor	$s2, $t0, $t1 # 9
+nor	$s3, $t0, $t1 # ...FF0
+add	$s4, $t0, $t1 # 15
+sub	$s5, $t0, $t1 # 7
+slt	$s6, $t0, $t1 # 1
+
+sw	$s0, 100($zero)
+sw	$s1, 104($zero)
+sw	$s2, 108($zero)
+sw	$s3, 112($zero)
+sw	$s4, 116($zero)
+sw	$s5, 120($zero)
+sw	$s6, 124($zero)
+
+sll	$s0, $t0, 7 # 700
+srl	$s1, $t0, 7 # 0
+sra	$s2, $t0, 7 # 0
+
+
+sw	$s1, 128($zero)
+sw	$s2, 132($zero)
+sw	$s3, 136($zero)
